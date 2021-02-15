3dac1ba9c90231d6227b5d0548708de2b63fb25b
dmezh <54985569+dmezh@users.noreply.github.com>
2021-02-14 20:24:23 -0500

    add findThreshold comments

--- hw3/findThreshold.m
+++ hw3/findThreshold.m
@@ -1,3 +1,6 @@
+% returns value and index of first element in x below some threshold
+% x must be strictly decreasing
+% for problem 2
 function [val, ind] = findThreshold(x, threshold)
 	    I = find(x<threshold, 1);
 	    [val, ind] = deal(x(I), I);
